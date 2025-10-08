13a5e59ba5001b1847ce6348d52cde0a1e2090aa
meivelan <meivelanmurugesan@gmail.com>
2025-10-08 09:28:13 +0530

    Ranking Metrics Declaration

---
+++ src/recommenders/evaluation/ranking_metrics.py
@@ -0,0 +1,14 @@
+def precision_at_k(relevancy_list, predicted_list, k):
+    pass
+
+def recall_at_k(relevancy_list, predicted_list, k):
+    pass
+
+def map():
+    pass
+
+def ncdg():
+    pass
+
+if __name__ == "__main__":
+    pass
