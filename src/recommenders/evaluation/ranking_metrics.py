46f0f227733d9dd98c4de72bfa3149ad38e18640
meivelan <meivelanmurugesan@gmail.com>
2025-10-08 14:38:43 +0530

    Precision@K and Recall@K Implementation

--- src/recommenders/evaluation/ranking_metrics.py
+++ src/recommenders/evaluation/ranking_metrics.py
@@ -1,8 +1,18 @@
 def precision_at_k(relevancy_list, predicted_list, k):
-    pass
+    relevant_items = 0
+    predicted_list = list(predicted_list)
+    for item in predicted_list[:k]:
+        if item in relevancy_list:
+            relevant_items += 1
+    return  relevant_items/k
 
 def recall_at_k(relevancy_list, predicted_list, k):
-    pass
+    relevant_items = 0
+    predicted_list = list(predicted_list)
+    for item in predicted_list[:k]:
+        if item in relevancy_list:
+            relevant_items += 1
+    return  relevant_items/len(relevancy_list)
 
 def map():
     pass
